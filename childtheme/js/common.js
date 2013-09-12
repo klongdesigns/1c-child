7c480f33b867aa2e0b82559668738112e8762223
Chris Klongpayabal <chrisk@1callservice.com>
2013-09-12 08:58:50 -0500

    Moving Footer Javascript to common.js file And deleting upload.js (not needed anymore)

---
+++ childtheme/js/common.js
@@ -0,0 +1,21 @@
+(function($) {
+$(document).foundation();
+
+$(document).ready(function() {
+/*
+//for prettyPhoto html5 code to be compliant, add this:
+	$('a[data-rel]').each(function() {
+		$(this).attr('rel', $(this).data('rel'));
+	});
+	$("a[rel^='prettyPhoto']").prettyPhoto({theme: 'facebook'});
+*/
+});
+
+$(window).load(function() {
+
+
+
+});
+
+
+})(jQuery);
