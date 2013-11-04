af5e93eeae40b341e06839b69a62e4cad4bc2496
Chris Klongpayabal <chrisk@1callservice.com>
2013-11-04 15:14:34 -0600

    Changed sidebar.php to have the correct sidebar listing

--- childtheme/sidebar.php
+++ childtheme/sidebar.php
@@ -2,9 +2,9 @@
 <!-- sidebar -->
 <div class="sidebar large-4 columns">
 
-	<?php if ( is_active_sidebar( 'right_sidebar' ) ) : ?>
+	<?php if ( is_active_sidebar( 'main_sidebar' ) ) : ?>
 
-		<?php dynamic_sidebar( 'right_sidebar' ); ?>
+		<?php dynamic_sidebar( 'main_sidebar' ); ?>
 
 	<?php endif; ?>
 
